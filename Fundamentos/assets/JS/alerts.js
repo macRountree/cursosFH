440d1962cbf0276db559d0f8211743a014ccd60b
Macrountree <a219206225@unison.mx>
2023-06-13 15:16:17 -0700

    seccion2 FH

---
+++ Fundamentos/assets/JS/alerts.js
@@ -0,0 +1,11 @@
+// alert("holsgj"); //eesta instruccion bloquea el navegador web es decir no se ejecuta el contenido HTML hasta que quitemos el alert
+
+// let name = prompt("tunombre");
+// console.log(name);
+// console.log("***" + name + " ****");
+
+// const seleccion = confirm("nani");
+// console.log(seleccion);
+
+console.log(global);
+//alert ,prompt confirm son funciones del objeto window
---
+++ Fundamentos/assets/JS/app.js
@@ -0,0 +1,15 @@
+alert("hola desde app");
+console.log("holamundo");
+//breakpoint en el navegador se detiene hasta la linea que seleccionamos
+let a = 20,
+  b = 10,
+  c = "hola ",
+  d = "spiderman";
+
+const saludo = c + d;
+
+c = "Hola, again";
+
+var outerWidth = 10000000;
+
+//let y const no sobreescriben  el objeto window cosa que si hace el var por eso ya no se utiliza
