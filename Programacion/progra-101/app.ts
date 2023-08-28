b351b6147f70ef71f4b3e677870c9c0e34fb2fbd
Macrountree <a219206225@unison.mx>
2023-08-28 15:37:57 -0700

    program

---
+++ Programacion/progra-101/app.ts
@@ -0,0 +1,16 @@
+
+
+let person = {
+    name:'Melissa',
+    age:45,
+    isActive:true,
+     hobbies: ['soccer', 'baseball'],
+     toString(){
+            let objectString = this.name + ' ' + this.age
+            console.log(objectString);
+    }
+    }
+    person.toString()
+    
+    
+    // console.log(person.name.toLocaleUpperCase());
