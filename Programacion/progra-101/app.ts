b705dbac931326c7c56ca68c45ac0c9819963d1b
Mac Rountree <a219206225@unison.mx>
2023-08-30 08:53:25 -0700

    clases

--- Programacion/progra-101/app.ts
+++ Programacion/progra-101/app.ts
@@ -10,13 +10,55 @@ class Car {
     constructor(){
         this.brand= 'No Brand';
         this.doors = 0;
-        this.fuelTank= 100;
+        this.fuelTank= 0;
         this.isRunning= false;
         this.type = 'No Type';
         this.createdAt= 546456;
     }
-}
+    //METODOS
+    turnOn(){
+    if(this.isRunning){
+      console.log('Carro Averiado');
+      return
+    }
+
+
+    if (this.fuelTank <=0) {
+      console.log('El carro no tiene gasolina');
+      return
+      
+    }
+    this.isRunning = true;
+    console.log('El carro esta encendido');
+  }
+
+  filTank(gas:number){
+
+    if ( gas <= 0) {
+      console.log('gas debe ser positivo');
+      return
+    }
+
+    let newFuelTank = this.fuelTank + gas;
+    if (newFuelTank >= 100){
+      this.fuelTank = 100;
+    }else{
+
+      this.fuelTank = newFuelTank
+      
+    }
+  }
+  }
 
 let myMazda = new Car();
+
+
+// console.log(myMazda);
+
+// myMazda.turnOn()
+
+myMazda.filTank(5)
+myMazda.filTank(96)
+myMazda.turnOn()
 
 console.log(myMazda);
